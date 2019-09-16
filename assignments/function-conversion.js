38b0ab2a3a3a29e4c67e86ad9de1c132005cb750
nijanes97 <nickjanes97@gmail.com>
2019-09-16 13:51:31 -0700

    finished javascript mvp and stretch

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,32 @@
 // };
 // myFunction();
 
+var myFunction = fnc => fnc;
+console.log(myFunction("Function was invoked"));
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+var anotherFunction = fnc => fnc;
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+var add = (param1, param2) => param1 + param2;
+add(1,2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+var subtract = (param1, param2) => param1 -param2;
+subtract(1, 2);
 
 // Stretch
 
@@ -28,3 +39,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+nums = [1,2,3,4];
+var triple = param1 => param1 * 3;
+console.log(nums.map(triple));
